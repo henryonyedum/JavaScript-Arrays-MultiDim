c7fa5f922f042769311d3a948ee82ea79c55a01f
Henry Onyedum <henryonyedum@gmail.com>
2021-02-12 12:08:27 -0500

    added psuedo code and for loop

---
+++ JavaScript/Multi.js
@@ -0,0 +1,16 @@
+/*create multi dementional Array with O's and one X
+define where the "winning " X
+prompt player "where the X?" 
+if player guess right console " you guessed right"
+if they guess wrong ask again 2x then console "you guessed wrong" 
+*/
+let multiArray=[
+ ["O","O","O"],
+ ["O","O","X"],
+ ["O","O","O"] 
+]
+
+let winningx= multiArray[1][2]
+
+for (let i=)
+let guess= prompt("guess where the X is")
